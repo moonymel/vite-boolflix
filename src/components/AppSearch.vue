efd2b89358dd1cd1b28056388ad69420dd2e133c
moonymel <musumeci.me@gmail.com>
2024-01-16 10:51:24 +0100

    [update] styled AppSearch.vue

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -11,13 +11,21 @@ export default {
 </script>
 
 <template lang="">
-    <div class="search d-flex justify-content-between p-3">
-        <div class="logo">
+    <div class="search d-flex justify-content-between align-items-center">
+        <div class="logo d-flex align-items-center">
             boolfix
+            <ul class="list-unstyled">
+                <li>Home</li>
+                <li>Serie TV</li>
+                <li>Film</li>
+                <li>Nuovi e popolari</li>
+                <li>La mia lista</li>
+                <li>Sfoglia per lista</li>
+            </ul>
         </div>
         <div class="search-bar">
             <input type="text" placeholder="Search" v-model="store.searchingKey" @keyup.enter="$emit('start_search')">
-            <button class="btn">Search</button>
+            <button class="btn btn-dark btn-sm mx-2">Search</button>
         </div>
     </div>
 </template>
@@ -27,10 +35,23 @@ export default {
 @use '../styles/partials/variables' as *;
 
 .search {
-    background-color: white;
+    background-color: black;
 
     .logo {
-        @include title
+        @include net_logo;
+
+        ul {
+
+            margin-left: 20px;
+
+            li {
+                display: inline-block;
+                color: white;
+                font-family: verdana;
+                font-size: 13px;
+                margin: 0px 7px;
+            }
+        }
     }
 }
 </style>
